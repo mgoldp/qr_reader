eec8748570e048be342e2b339d32542ddfb66508
mgoldp <mgoldp2013@gmail.com>
2022-05-10 23:02:18 -0400

    SQFLite configuration

--- lib/models/scan_model.dart
+++ lib/models/scan_model.dart
@@ -6,10 +6,10 @@ class ScanModel {
     required this.tipo,
     required this.valor,
   }) {
-    if (this.tipo.contains('http')) {
-      this.tipo = 'http';
+    if (tipo.contains('http')) {
+      tipo = 'http';
     } else {
-      this.tipo = 'geo';
+      tipo = 'geo';
     }
   }
 
