f858f335f0e8b193f95dcb171d26836e6bd47136
mgoldp <mgoldp2013@gmail.com>
2022-05-15 01:53:25 -0400

    ScanListPovider centralize scan information

---
+++ lib/providers/scan_list_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+
+class ScanListProvider extends ChangeNotifier {
+  List<ScanModel> scans = [];
+  String tipoSeleccionado = 'http';
+
+  nuevoScan(String valor) async {
+    final nuevoScan = ScanModel(valor: valor);
+    final id = await DBProvider.db.nuevoScan(nuevoScan);
+
+    // asignar el ID de la base de datos al modelo
+    nuevoScan.id = id;
+
+    if (this.tipoSeleccionado == nuevoScan.tipo) {
+      this.scans.add(nuevoScan);
+      notifyListeners();
+    }
+  }
+}
