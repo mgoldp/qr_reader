93d58e5a6c8acdac010efd590a707fe4dc821ad1
mgoldp <mgoldp2013@gmail.com>
2022-06-01 18:02:59 -0400

    refactor of the code in LisTiles

--- lib/pages/direcciones_page.dart
+++ lib/pages/direcciones_page.dart
@@ -1,30 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/widgets/scan_tides.dart';
 
 class DireccionesPage extends StatelessWidget {
   const DireccionesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-    final scans = scanListProvider.scans;
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, i) => ListTile(
-        leading: Icon(
-          Icons.home_max_outlined,
-          color: Theme.of(context).primaryColor,
-        ),
-        title: Text(scans[i].valor),
-        subtitle: Text(scans[i].id.toString()),
-        trailing: const Icon(
-          Icons.keyboard_arrow_right,
-          color: Colors.grey,
-        ),
-        onTap: () => print('Abrir Algo'),
-      ),
-    );
+    return const ScanTiles(tipo: 'http');
   }
 }
---
+++ lib/widgets/scan_tides.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/scan_list_provider.dart';
+
+class ScanTiles extends StatelessWidget {
+  const ScanTiles({
+    Key? key,
+    required this.tipo,
+  }) : super(key: key);
+
+  final String tipo;
+
+  @override
+  Widget build(BuildContext context) {
+    final scanListProvider = Provider.of<ScanListProvider>(context);
+    final scans = scanListProvider.scans;
+
+    return ListView.builder(
+      itemCount: scans.length,
+      itemBuilder: (_, i) => Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          color: Colors.deepPurple.shade800,
+        ),
+        onDismissed: (DismissDirection direction) {
+          final swipeScanListProvider =
+              Provider.of<ScanListProvider>(context, listen: false);
+          swipeScanListProvider.borrarScanPorId(scans[i].id!);
+        },
+        child: ListTile(
+          leading: Icon(
+            tipo == 'http' ? Icons.link_outlined : Icons.map_outlined,
+            color: Theme.of(context).primaryColor,
+          ),
+          title: Text(scans[i].valor),
+          subtitle: Text(scans[i].id.toString()),
+          trailing: const Icon(
+            Icons.keyboard_arrow_right,
+            color: Colors.grey,
+          ),
+          onTap: () => print('Abrir Algo'),
+        ),
+      ),
+    );
+  }
+}
